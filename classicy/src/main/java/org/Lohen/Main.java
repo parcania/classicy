0e328a6d953f824c9f5ecbaf2ddbd60725a87183
parcania <kinloh14@evergreen.edu>
2023-11-21 16:14:04 -0800

    Testing CSV arraylist from file input

--- classicy/src/main/java/org/Lohen/Main.java
+++ classicy/src/main/java/org/Lohen/Main.java
@@ -1,21 +1,24 @@
 package org.Lohen;
+
 import java.io.*;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> myList = new ArrayList<>();
+        ArrayList<String> myList = new ArrayList<>(); //create an array to add info from the CSV to
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("src/main/java/org/Lohen/PRECIP_HLY_sample_csv.csv"));
+            BufferedReader reader = new BufferedReader(new FileReader("src/main/java/org/Lohen/030750.csv"));
             String line = reader.readLine();
-            while (line != null) {
-                myList.add(line);
+            while (line != null) {                       //as long as there is still content
+                myList.add(line);                       //add content to arraylist myList
                 line = reader.readLine();
             }
             reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(myList);
+        for (int i = 0; i < myList.size(); i++) {
+            System.out.println(myList.get(i));
+        }
     }
 }
